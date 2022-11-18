ab43800986bc727e6230c354aece8d72e70b2945
7jie <hbk8816408@gmail.com>
2022-11-18 12:00:30 +0800

    t

--- app.py
+++ app.py
@@ -1329,4 +1329,4 @@ def getadit_re():
     return "OK"
 
 if __name__=='__main__':
-    app.run('0.0.0.0',debug=True,port='8000') #120.110.7.178 
+    app.run('120.110.7.178',debug=True,port='7000') #120.110.7.178 
